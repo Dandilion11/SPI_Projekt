0feb69c7f841fc5472e3473f622731cfd444e3e8
Peter Stein <peter.stein@campus.tu-berlin.de>
2026-07-05 21:53:57 +0200

    Main1 / Main2 um Modell2 erweitert

--- main/main_01_data_preprocessing.m
+++ main/main_01_data_preprocessing.m
@@ -15,12 +15,15 @@ exp_03 = daten_03.Mess;
 exp_04 = daten_04.Mess;
 
 
-% 1. Koeffizienten für Varianzberechnung definieren
+% 1. Koeffizienten für Varianzberechnung definieren -> GEGENCHECKEN!!!
 a_bio = 0.02; b_bio = 0.015;
 a_glc = 0.06; b_glc = 0.25;
 
 a_o2 = 0.02; 
 b_o2 = 0.5;
+
+a_am   = 0.05; b_am   = 0.02;
+a_base = 0.05; b_base = 0.01;
 
 % 2. Funktion zum Filtern und Varianz berechnen (t <= 20h)
 filterAndVarBatch = @(t, y, a, b) struct('t', t(t <= 20), ...
@@ -40,6 +43,12 @@ TrainData.O2 = filterAndVarBatch(exp_03.Messdaten.O2.BatchAge, ...
     exp_03.Messdaten.O2.Wert, ...
     a_o2, b_o2);
 
+TrainData.Ammonium = filterAndVarBatch(exp_03.Messdaten.Ammonium.BatchAge, ...
+    exp_03.Messdaten.Ammonium.Wert, a_am, b_am);
+
+TrainData.Base = filterAndVarBatch(exp_03.Messdaten.BASE.BatchAge, ...
+    exp_03.Messdaten.BASE.Wert, a_base, b_base);
+
 % 4. Validierungsdaten extrahieren und Varianz berechnen (RamScDef04)
 ValData.Biomasse = filterAndVarBatch(exp_04.Messdaten.Biomasse.BatchAge, ...
     exp_04.Messdaten.Biomasse.Wert, ...
@@ -53,6 +62,11 @@ ValData.O2 = filterAndVarBatch(exp_04.Messdaten.O2.BatchAge, ...
     exp_04.Messdaten.O2.Wert, ...
     a_o2, b_o2);
 
+ValData.Ammonium = filterAndVarBatch(exp_04.Messdaten.Ammonium.BatchAge, ...
+    exp_04.Messdaten.Ammonium.Wert, a_am, b_am);
+
+ValData.Base = filterAndVarBatch(exp_04.Messdaten.BASE.BatchAge, ...
+    exp_04.Messdaten.BASE.Wert, a_base, b_base);
 
 % Dichte p = 1 kg/L wird angenommen. Daher: Volumen (L) = Gewicht (kg)
 TrainData.V = exp_03.Messdaten.Weight.Wert(1);
@@ -75,4 +89,4 @@ fprintf('Daten erfolgreich gespeichert in:\n%s\n', savePath);
 
 
 
-disp('Trainings- und Validierungsdaten ink. o2 erfolgreich geladen und auf t <= 20h beschnitten.');
+disp('Trainings- und Validierungsdaten erfolgreich geladen und auf t <= 20h beschnitten.');
--- main/main_02_simulation.m
+++ main/main_02_simulation.m
@@ -8,7 +8,7 @@ projectRoot = pwd;
 addpath(fullfile(projectRoot, '..','Modelle'),'-begin');
 
 rehash;
-clear Modell1
+clear Modell1 Modell2
 
 if ~isfile(loadPath)
     error('Data not found.\n');
@@ -17,20 +17,25 @@ else
 end 
 load(loadPath);
 
-t_bio   = TrainData.Biomasse.t;
-y_bio   = TrainData.Biomasse.y;
-var_bio = TrainData.Biomasse.var;
+Data = TrainData; % Geht auch ValData -> zum schnelleren wechseln
 
-t_glc   = TrainData.Glucose.t;      
-y_glc   = TrainData.Glucose.y;
-var_glc = TrainData.Glucose.var;
+t_bio   = Data.Biomasse.t;
+y_bio   = Data.Biomasse.y;
+var_bio = Data.Biomasse.var;
 
-t_o2 = TrainData.O2.t;
-y_o2 = TrainData.O2.y;
-var_o2 = TrainData.O2.var;
+t_glc   = Data.Glucose.t;      
+y_glc   = Data.Glucose.y;
+var_glc = Data.Glucose.var;
 
-%% 2. Define Initial Conditions and Parameters
-% Modell1 expects concentrations (cX, cGlc), not masses.
+t_o2 = Data.O2.t;
+y_o2 = Data.O2.y;
+var_o2 = Data.O2.var;
+
+%% A2. Anfangswerte und Parameter
+kinetic = 3; % 3 = Monod
+%--------------------------------------------------------------------------
+withOxygen = true; 
+%--------------------------------------------------------------------------
 % Anfangswerte aus der jeweils ersten Messung.
 x0 = [y_bio(1); y_glc(1)];
 
@@ -39,12 +44,9 @@ x0 = [y_bio(1); y_glc(1)];
 p_guess = [0.3, 0.5, 0.15]; 
 
 % Model Configuration Flags
-kinetic = 3; % 3 = Monod
-%--------------------------------------------------------------------------
-withOxygen = true; % Exclude O2 state and parameters
-%--------------------------------------------------------------------------
+
 if withOxygen
-    cO2_0    = 95;     % [%]     Start-DOT (aus O2-Messung zu Batch-Beginn)
+    cO2_0    = y_o2(1);     % [%]     Start-DOT (aus O2-Messung zu Batch-Beginn)
     YXO      = 1.0;    % [gX/gO2] Ertragskoeffizient Biomasse/Sauerstoff (Schaetzwert)
     KLa      = 200;    % [1/h]    volumetrischer O2-Transferkoeffizient (Schaetzwert)
     cO2stern = 100;    % [%]      Saettigungs-DOT (Gleichgewicht mit Gasphase)
@@ -55,7 +57,7 @@ end
 
 
 
-%% 3. Execute Simulation
+%% A3. Execute Simulation
 % Simulation nur so lange laufen lassen wie Daten vorhanden sind:
 t_end = max([t_bio(:)+1; t_glc(:)+1]);
 t_sim = linspace(0, t_end, 200);
@@ -66,7 +68,7 @@ options = odeset('RelTol', 1e-5, 'AbsTol', 1e-7);
 [~, X_sim] = ode45(@(t, x) Modell1(t, x, p_guess, kinetic, withOxygen), t_sim, x0, options);
 fprintf('[OK] Simulation \n');
 
-%% 4. Extract Data
+%% A4. Extract Data
 % Division by V is no longer necessary as Modell1 outputs concentrations directly.
 c_X_sim   = X_sim(:, 1);
 c_Glc_sim = X_sim(:, 2);
@@ -74,7 +76,7 @@ if withOxygen
     cO2_sim = X_sim(:, 3);
 end
 
-%% 5. Plot Results
+%% A5. Plot Results
 figure('Name', 'Simulation Test (Modell1)', 'Position', [200, 200, 900, 600]);
 if withOxygen
     nRows = 3;
@@ -114,3 +116,79 @@ if withOxygen
     xlim([0 9.5])
     grid on;
 end
+
+
+
+
+%% MODELL 2 Geht ab hier los 
+%% B6. Anfangswerte und Parameter
+clear Modell2
+
+x0_m2 = [Data.Biomasse.y(1); Data.Glucose.y(1); Data.Ammonium.y(1); Data.Base.y(1); Data.O2.y(1)];
+
+% Startschaetzwerte: GEGENCHECKEN!!!!!
+%   YBam = Yield Base/Ammonium, YAmX = Yield Ammonium/Biomasse
+p_guess_m2 = [0.3, 0.5, 0.15, 1.0, 0.05, 1.0, 200, 100];
+
+%% B7. Simulation
+t_end_m2 = max([Data.Biomasse.t(:)+1; Data.Glucose.t(:)+1; Data.Ammonium.t(:)+1; Data.Base.t(:)+1; Data.O2.t(:)+1]);
+
+t_sim_m2 = linspace(0, t_end_m2, 300);
+
+[~, X2]  = ode45(@(t, x) Modell2(t, x, p_guess_m2, kinetic), t_sim_m2, x0_m2, options);
+
+
+%% B8. Plot Modell2
+figure('Name','Simulation Test (Modell2: +Base +O2)','Position',[250 80 950 950]);
+
+subplot(5,1,1);
+errorbar(Data.Biomasse.t, Data.Biomasse.y, sqrt(Data.Biomasse.var)); hold on;
+plot(t_sim_m2, X2(:,1), 'LineWidth', 2);
+title('Modell2 - Biomasse'); 
+ylabel('c_X (g/L)'); 
+legend("Messung \pm \sigma","Simulation"); 
+xlim([0 10])
+grid on;
+
+subplot(5,1,2);
+errorbar(Data.Glucose.t, Data.Glucose.y, sqrt(Data.Glucose.var)); hold on;
+plot(t_sim_m2, X2(:,2), 'LineWidth', 2);
+title('Modell2 - Glucose'); 
+ylabel('c_{Glc} (g/L)');  
+xlim([0 10])
+legend("Messung \pm \sigma","Simulation");
+grid on;
+
+subplot(5,1,3);
+errorbar(Data.Ammonium.t, Data.Ammonium.y, sqrt(Data.Ammonium.var)); hold on;
+plot(t_sim_m2, X2(:,3), 'LineWidth', 2);
+title('Modell2 - Ammonium'); 
+ylabel('c_{Am} (g/L)'); 
+xlim([0 10])
+legend("Messung \pm \sigma","Simulation");
+grid on;
+
+subplot(5,1,4);
+errorbar(Data.Base.t, Data.Base.y, sqrt(Data.Base.var)); hold on;
+plot(t_sim_m2, X2(:,4), 'LineWidth', 2);
+title('Modell2 - Base'); 
+ylabel('c_{Base}'); 
+xlim([0 10])
+legend("Messung \pm \sigma","Simulation");
+grid on;
+
+subplot(5,1,5);
+errorbar(Data.O2.t, Data.O2.y, sqrt(Data.O2.var)); hold on;
+plot(t_sim_m2, X2(:,5), 'LineWidth', 2);
+title('Modell2 - Sauerstoff (DOT)'); 
+ylabel('cO_2 (%)'); 
+xlabel('BatchAge (h)');
+xlim([0 10])
+legend("Messung \pm \sigma","Simulation");
+grid on;
+
+
+
+
+
+
